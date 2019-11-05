886cc59bc91a3c3c3a5bab66789dd32a8e82bfb3
htchenboulder <51387181+htchenboulder@users.noreply.github.com>
2019-11-05 15:45:56 -0700

    Add files via upload

--- test-summit/job-summmit.sh
+++ test-summit/job-summmit.sh
@@ -14,10 +14,14 @@ mkdir -p matrix
 mkdir -p out
 mkdir -p dump
 
+#cp ~walkup/bin/numactl .
 #OpenACC option
 #export PGI_ACC_TIME=1
+#export RANKS_PER_NODE=42
 #aprun -n 4 pgprof -o gem.%p.prof ./gem_main >& run.out
-#nvprof option
-#jsrun -n 24 -a 7 -g 1 -c 7 nvprof -s --openmp-profiling off --output-profile gem.%p.prof ./gem_main > run.out 2> run.err
+##nvprof option: p means number of mpi, openmp-profiling off means only gpu timeline will be reported
+#jsrun -n 96 -r 6 -a 7 -g 1 -c 7 nvprof -s --openmp-profiling off --output-profile gem.%p.prof ./gem_main > run.out 2> run.err
 #pure option
-jsrun -n 384 -a 7 -g 1 -c 7 ./gem_main  > run.out 2>run.err
+jsrun -n 384 -r 6 -a 7 -g 1 -c 7 ./gem_main > run.out 2>run.err
+#jsrun --smpiargs="-gpu" -n 24 -r 6 -a 7 -g 1 -c 7 ~walkup/bin/h6smt1.sh ./gem_main > run.out 2>run.err
+#jsrun --smpiargs="-gpu" -n 8 -r 1 -a 42 -g 6 -c 42 ~walkup/bin/h6smt1.sh ./gem_main > run.out 2>run.err
