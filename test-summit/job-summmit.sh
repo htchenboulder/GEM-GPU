47698d32b18e75c095eac8e0c0e4cfe32fbc93b9
htchenboulder <51387181+htchenboulder@users.noreply.github.com>
2020-03-19 16:57:40 -0600

    Add files via upload

--- test-summit/job-summmit.sh
+++ test-summit/job-summmit.sh
@@ -2,7 +2,7 @@
 
 #BSUB -P phy122
 #BSUB -W 0:30
-#BSUB -nnodes 64
+#BSUB -nnodes 4
 #BSUB -alloc_flags gpumps
 #BSUB -J gem_test
 #BSUB -o gem_test.%J
@@ -19,9 +19,11 @@ mkdir -p dump
 #export PGI_ACC_TIME=1
 #export RANKS_PER_NODE=42
 #aprun -n 4 pgprof -o gem.%p.prof ./gem_main >& run.out
-##nvprof option: p means number of mpi, openmp-profiling off means only gpu timeline will be reported
+##nvprof option is following this line: p means number of mpi, openmp-profiling off means only gpu timeline will be reported
 #jsrun -n 96 -r 6 -a 7 -g 1 -c 7 nvprof -s --openmp-profiling off --output-profile gem.%p.prof ./gem_main > run.out 2> run.err
 #pure option
-jsrun -n 384 -r 6 -a 7 -g 1 -c 7 ./gem_main > run.out 2>run.err
+jsrun -n 24 -r 6 -a 7 -g 1 -c 7 ./gem_main > run.out 2>run.err
+#tau profile
+#jsrun -n 24 -r 6 -a 7 -g 1 -c 7 tau_exec -openacc ./gem_main > run.out 2>run.err
 #jsrun --smpiargs="-gpu" -n 24 -r 6 -a 7 -g 1 -c 7 ~walkup/bin/h6smt1.sh ./gem_main > run.out 2>run.err
 #jsrun --smpiargs="-gpu" -n 8 -r 1 -a 42 -g 6 -c 42 ~walkup/bin/h6smt1.sh ./gem_main > run.out 2>run.err
