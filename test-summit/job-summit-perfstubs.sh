4b97eb45b63d3dc1bf3d311e85a03aadaf8361a7
htchenboulder <51387181+htchenboulder@users.noreply.github.com>
2020-04-17 11:16:25 -0600

    Add files via upload
    modified for mpi=2 case

--- test-summit/job-summit-perfstubs.sh
+++ test-summit/job-summit-perfstubs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#BSUB -P csc297
+#BSUB -P phy122
 #BSUB -W 0:30
-#BSUB -nnodes 4
+#BSUB -nnodes 1
 #BSUB -alloc_flags gpumps
 #BSUB -J gem_test
 #BSUB -o gem_test.%J
@@ -23,7 +23,8 @@ mkdir -p dump
 #jsrun -n 96 -r 6 -a 7 -g 1 -c 7 nvprof -s --openmp-profiling off --output-profile gem.%p.prof ./gem_main > run.out 2> run.err
 #pure option
 module unload darshan-runtime
-jsrun -n 24 -r 6 -a 7 -g 1 -c 7 ./gem_main_perfstubs > run.perfstubs.out 2>run.perfstubs.err
+jsrun -n 2 -r 2 -a 1 -g 1 -c 1 ./gem_main > run.perfstubs.out 2>run.perfstubs.err
+#jsrun -n 24 -r 6 -a 7 -g 1 -c 7 ./gem_main_perfstubs > run.perfstubs.out 2>run.perfstubs.err
 #tau profile
 #jsrun -n 24 -r 6 -a 7 -g 1 -c 7 tau_exec -openacc ./gem_main > run.out 2>run.err
 #jsrun --smpiargs="-gpu" -n 24 -r 6 -a 7 -g 1 -c 7 ~walkup/bin/h6smt1.sh ./gem_main > run.out 2>run.err
